aec52fad364a61c246c59b234fc02291ae8350bc
Ingkli Boja <76855285+ingli0@users.noreply.github.com>
2024-01-18 20:23:41 +0200

    Add files via upload

---
+++ HandTrackingMin.py
@@ -0,0 +1,23 @@
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+
+while True:
+    succes, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(img,handLms,mpHands.HAND_CONNECTIONS)
+
+
+    cv2.imshow("image",img)
+    cv2.waitKey(1)
