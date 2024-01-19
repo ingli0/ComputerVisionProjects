86d1216b5c4d260bc93954b698a8340640f0011c
ingli0 <inbogia@cs.ihu.gr>
2024-01-19 18:26:52 +0200

    face mesh added

--- faceMesh/faceMeshBasics.py
+++ faceMesh/faceMeshBasics.py
@@ -20,7 +20,7 @@ while True:
             mpDraw.draw_landmarks(img,faceLms,mpFaceMesh.FACEMESH_CONTOURS,
                                   drawSpec,drawSpec)
             for id,lm in enumerate(faceLms.landmark):
-                #print(lm)
+                #print(lm) 
                 ih,iw,ic= img.shape
                 x,y = int(lm.x*iw),int(lm.y*ih)
                 print(id,x,y)
