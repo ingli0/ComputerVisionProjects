9a02732d54e0a14edd8f4646f02578f466162dd4
ingli0 <inbogia@cs.ihu.gr>
2024-01-19 16:23:20 +0200

    pose updated

---
+++ Pose/poseModule.py
@@ -0,0 +1,69 @@
+import cv2
+import mediapipe as mp
+import time
+
+class poseDetector():
+
+    def __init__(self, mode=False, upBody=False, smooth=True,
+                 detectionCon=0.5, trackCon=0.5):
+
+        self.mode = mode
+        self.upBody = upBody
+        self.smooth = smooth
+        self.detectionCon = detectionCon
+        self.trackCon = trackCon
+
+        self.mpDraw = mp.solutions.drawing_utils
+        self.mpPose = mp.solutions.pose
+        self.pose = self.mpPose.Pose(static_image_mode=self.mode,
+                                     smooth_landmarks=self.smooth,
+                                     min_detection_confidence=self.detectionCon,
+                                     min_tracking_confidence=self.trackCon)
+
+    def findpose(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.pose.process(imgRGB)
+        if self.results.pose_landmarks:
+            if draw:
+                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
+                                           self.mpPose.POSE_CONNECTIONS)
+        return img
+
+    def getPosition(self,img,draw=True):
+        lmList = []
+        if self.results.pose_landmarks:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                h,w,c=img.shape
+                #print(id,lm)
+                cx,cy=int(lm.x * w),int(lm.y* h)
+                lmList.append([id,cx,cy])
+                if draw:
+                    cv2.circle(img,(cx,cy),5,(255,0,0),cv2.FILLED)
+        return lmList
+
+
+def main():
+    cap = cv2.VideoCapture('1.mp4')
+    pTime = 0
+    detector = poseDetector()
+    while True:
+
+        success, img = cap.read()
+        img=detector.findpose(img)
+        lmList = detector.getPosition(img)
+        print(lmList)
+
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+        if len(lmList) != 0:
+            cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+        cv2.imshow("Image", img)
+        cv2.waitKey(1)
+
+
+
+
+if __name__ == "__main__":
+    main()
---
+++ Pose/poseUse.py
@@ -0,0 +1,20 @@
+import cv2
+import time
+import poseModule as pm
+
+cap = cv2.VideoCapture('1.mp4')
+pTime = 0
+detector = pm.poseDetector()
+while True:
+    success, img = cap.read()
+    img = detector.findpose(img)
+    lmList = detector.getPosition(img)
+    print(lmList)
+    cTime = time.time()
+    fps = 1 / (cTime - pTime)
+    pTime = cTime
+    if len(lmList) != 0:
+        cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
