ec55ed6c4b5c867e8791e2e4097fc6224182c1a7
ingli0 <inbogia@cs.ihu.gr>
2024-01-26 21:09:55 +0200

    virtual mouse

--- hand/mouseVirtual.py
+++ hand/mouseVirtual.py
@@ -4,5 +4,16 @@ import handTrackingModule as htm
 import time
 import autopy
 
+######################
+wCam, hCam = 640,480
+####################
+
+
 cap = cv2.VideoCapture(0)
-cap.set(3,640)
+cap.set(3,wCam)
+cap.set(4,hCam)
+
+while True:
+    success, img = cap.read()
+    cv2.imshow("Image",img)
+    cv2.waitKey(1)
